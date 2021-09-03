6122012526df9c6306a7118ddf8e5a6c0ba2f357
Matthias Dillen <matthias.dillen@inuits.eu>
2021-09-03 11:59:31 +0200

    Refs 82001 edit setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+
 setup(
     name='inuits-dams-job-helper',
-    packages=find_packages(include=['inuits-dams-job-helper']),
+    packages=['inuits-dams-job-helper'],
     version='0.1.0',
     description='Job helper to use with the job api',
     author='matthias.dillen@inuits.eu',
     license='MIT',
     install_requires=[],
+    provides=['inuits-dams-job-helper']
 )
