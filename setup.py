63406948336f91ffae6390f1099469a2f517915b
Matthias Dillen <matthias.dillen@inuits.eu>
2021-09-03 12:10:14 +0200

    Refs 82001 edit setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(
     version='0.1.0',
     description='Job helper to use with the job api',
     author='matthias.dillen@inuits.eu',
-    license='MIT',
-    install_requires=[]
+    license='MIT'
 )
