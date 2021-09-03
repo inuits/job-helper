606312174bcce597c1b0364df1105a3d9930e3d2
Matthias Dillen <matthias.dillen@inuits.eu>
2021-09-03 12:04:12 +0200

    Refs 82001 edit setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='inuits-dams-job-helper',
-    packages=['inuits-dams-job-helper'],
+    name='dams-job-helper',
+    packages=['dams-job-helper'],
     version='0.1.0',
     description='Job helper to use with the job api',
     author='matthias.dillen@inuits.eu',
