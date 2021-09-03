f6d602c83e1fb48713cb79dabd308afd5e704416
Matthias Dillen <matthias.dillen@inuits.eu>
2021-09-03 11:21:11 +0200

    Refs 82001 add missing files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='inuits-dams-job-helper',
+    packages=find_packages(include=['inuits-dams-job-helper']),
+    version='0.1.0',
+    description='Job helper to use with the job api',
+    author='matthias.dillen@inuits.eu',
+    license='MIT',
+    install_requires=[],
+)
