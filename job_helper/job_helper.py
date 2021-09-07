f7f19d4c5f6a8b89ed0ae214118ba95b2408f66d
Matthias Dillen <matthias.dillen@inuits.eu>
2021-09-07 13:40:12 +0200

    Add amount of completed jobs counter

--- job_helper/job_helper.py
+++ job_helper/job_helper.py
@@ -32,25 +32,32 @@ class JobHelper:
             "user": g.oidc_token_info["email"] if hasattr(g, "oidc_token_info") else "default_uploader",
             "asset_id": "" if asset_id is None else asset_id,
             "mediafile_id": "" if mediafile_id is None else mediafile_id,
-            "parent_job_id": "" if parent_job_id is None else parent_job_id
+            "parent_job_id": "" if parent_job_id is None else parent_job_id,
+            "completed_jobs": 0,
+            "amount_of_jobs": 1
         }
         job = json.loads(requests.post(
             "{}/jobs".format(self.job_api_base_url), json=new_job
         ).text)
         return job
 
-    def progress_job(self, job, asset_id=None, mediafile_id=None, parent_job_id=None):
+    def progress_job(self, job, asset_id=None, mediafile_id=None, parent_job_id=None, amount_of_jobs=None, count_up_completed_jobs=False):
         if asset_id is not None:
             job["asset_id"] = asset_id
         if mediafile_id is not None:
             job["mediafile_id"] = mediafile_id
         if parent_job_id is not None:
             job["parent_job_id"] = parent_job_id
+        if amount_of_jobs is not None:
+            job["amount_of_jobs"] = amount_of_jobs
+        if count_up_completed_jobs:
+            job["completed_jobs"] = job["completed_jobs"] + 1
         job["status"] = Status.IN_PROGRESS.value
         return self.__patch_job(job)
 
     def finish_job(self, job):
         job["status"] = Status.FINISHED.value
+        job["completed_jobs"] = job["amount_of_jobs"]
         job["end_time"] = str(datetime.utcnow())
         return self.__patch_job(job)
 
