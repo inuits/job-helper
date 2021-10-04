4a39fb7e95c98e80268d0f0314c35740b926311b
Matthias Dillen <matthias.dillen@inuits.eu>
2021-10-04 12:25:26 +0200

    Edit license description

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='job_helper',
-    version='0.1.6',
+    version='0.1.7',
     description="Job helper to use with the job api",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GPLv2 License',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3'
     ],
