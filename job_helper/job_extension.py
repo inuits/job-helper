d2af12f4a07eb725e84a6aa2bcba4c06f2cce83e
Matthias Dillen <matthias.dillen@inuits.eu>
2022-08-09 13:22:14 +0200

    Non blocking stateful jobs

--- job_helper/job_extension.py
+++ job_helper/job_extension.py
@@ -3,8 +3,10 @@ import string
 import uuid
 from datetime import datetime
 from enum import Enum
+
+from cloudevents.http import CloudEvent, to_json
 from flask import g
-import requests
+from rabbitmq_pika_flask import RabbitMQ
 
 
 class Status(Enum):
@@ -14,43 +16,14 @@ class Status(Enum):
     FAILED = "failed"
 
 
-class JobHelper:
-    def __init__(self, job_api_base_url, static_jwt=False):
-        self.job_api_base_url = job_api_base_url
-        self.headers = (
-            {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(static_jwt),
-            }
-            if static_jwt is not False
-            else {"Content-Type": "application/json"}
-        )
-
-    def __patch_job(self, job):
-        return requests.patch(
-            "{}/jobs/{}".format(
-                self.job_api_base_url, job["_key"] if "_key" in job else job["_id"]
-            ),
-            json=job,
-            headers=self.headers,
-        ).json()
-
-    def __get_parent_job(self, job):
-        return requests.get(
-            "{}/jobs/{}".format(self.job_api_base_url, job["parent_job_id"]),
-            headers=self.headers,
-        ).json()
-
-    def __get_job(self, id):
-        return requests.get(
-            "{}/jobs/{}".format(self.job_api_base_url, id), headers=self.headers
-        ).json()
+class JobExtension:
+    def __init__(self, rabbit: RabbitMQ):
+        self.rabbit = rabbit
 
     def create_new_job(
         self,
         job_info: string,
         job_type: string,
-        identifier: uuid.uuid1,
         asset_id=None,
         mediafile_id=None,
         parent_job_id=None,
@@ -68,27 +41,21 @@ class JobHelper:
             "parent_job_id": "" if parent_job_id is None else parent_job_id,
             "completed_jobs": 0,
             "amount_of_jobs": 1,
-            "identifiers": [identifier],
+            "identifiers": [uuid.uuid1()],
         }
-        job = json.loads(
-            requests.post(
-                "{}/jobs".format(self.job_api_base_url),
-                json=new_job,
-                headers=self.headers,
-            ).text
-        )
-        return job
+        self.__send_cloud_event(new_job, "dams.job_created")
+        return new_job
 
     def progress_job(
         self,
-        job_identifier,
+        job,
         asset_id=None,
         mediafile_id=None,
         parent_job_id=None,
         amount_of_jobs=None,
         count_up_completed_jobs=False,
     ):
-        job = self.__get_job(job_identifier)
+
         if asset_id is not None:
             job["asset_id"] = asset_id
         if mediafile_id is not None:
@@ -100,21 +67,27 @@ class JobHelper:
         if count_up_completed_jobs:
             job["completed_jobs"] = job["completed_jobs"] + 1
         job["status"] = Status.IN_PROGRESS.value
-        return self.__patch_job(job)
+        self.__send_cloud_event(job, "dams.job_changed")
+        return job
 
-    def finish_job(self, job_identifier):
-        job = self.__get_job(job_identifier)
+    def finish_job(self, job, parent_job=None):
         job["status"] = Status.FINISHED.value
         job["completed_jobs"] = job["amount_of_jobs"]
         job["end_time"] = str(datetime.utcnow())
-        if job["parent_job_id"] is not "":
-            parent_job = self.__get_parent_job(job)
+        if job["parent_job_id"] not in ["", None] and parent_job is not None:
             self.progress_job(parent_job, count_up_completed_jobs=True)
-        return self.__patch_job(job)
+        self.__send_cloud_event(job, "dams.job_changed")
+        return job
 
-    def fail_job(self, job_identifier, error_message=""):
-        job = self.__get_job(job_identifier)
+    def fail_job(self, job, error_message=""):
         job["status"] = Status.FAILED.value
         job["end_time"] = str(datetime.utcnow())
         job["error_message"] = error_message
-        return self.__patch_job(job)
+        self.__send_cloud_event(job, "dams.job_changed")
+        return job
+
+    def __send_cloud_event(self, job, event_type):
+        attributes = {"type": event_type, "source": "dams"}
+        event = CloudEvent(attributes, job)
+        message = json.loads(to_json(event))
+        self.rabbit.send(message, routing_key=event_type)
